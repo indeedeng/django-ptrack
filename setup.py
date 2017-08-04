6e23be84bcccb2aa4e89c1e56f565f0f13f59560
Richard Latimer <rlatimer@indeed.com>
2017-08-04 18:11:43 -0500

    v1.1.0: Prepare django-ptrack for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,37 @@
 __author__ = "Richard Latimer"
 from setuptools import setup, find_packages
+from os import path
 
-version = '1.0.0'
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='django-ptrack',
-    version=version,
+    version='1.1.0',
+    description='A sample Python project',
+    long_description=long_description,
+
+    url='https://github.com/indeedeng/django-ptrack',
+
+    author='Richard Latimer, Sarah Ellis',
+    author_email='rlatimer@indeed.com, sellis@indeed.com',
+
+    license='Apache',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Pixel Tracking',
+        'License :: Apache License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='django',
+
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'example_project']),
     install_requires=[
         'Django>=1.8.1',
