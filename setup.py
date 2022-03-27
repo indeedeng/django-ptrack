68f4c98f0c568d8226843dd033c633e977217b13
stevenmiller <stevenmiller@indeed.com>
2022-03-27 10:29:22 -0700

    DJANGO-1511: Updated Django requirements and fixed a flake8 warning

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='django-ptrack',
-    version='2.2.2',
+    version='3.0.0',
     description='Ptrack is a tracking pixel library for Django',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -27,14 +27,8 @@ setup(
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
@@ -43,7 +37,7 @@ setup(
 
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'example_project']),
     install_requires=[
-        'Django>=1.8.1',
+        'Django>=2.0.0',
         'pynacl>=1.0.1'
     ],
     extras_require={
