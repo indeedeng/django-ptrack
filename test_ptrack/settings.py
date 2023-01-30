1a243d0392d38d15ae976a6a72022680fe68a838
campbell <campbell@dxclabs.com>
2023-01-30 07:36:44 +0000

    reformat test_ptrack

--- test_ptrack/settings.py
+++ test_ptrack/settings.py
@@ -1,38 +1,32 @@
-SECRET_KEY = 'ptracktestsecret'
-STATIC_URL = '/'
+"""Settings for ptrack tests."""
+SECRET_KEY = "ptracktestsecret"  # nosec B105
+STATIC_URL = "/"
 
 # Is this required?
 # ROOT_URLCONF = 'ptrack.urls'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'ptrack_test.db',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "ptrack_test.db",
     }
 }
 
+INSTALLED_APPS = ["django.contrib.auth", "django.contrib.contenttypes", "django.contrib.staticfiles", "ptrack"]
 
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.staticfiles',
-    'ptrack'
-]
-
-ROOT_URLCONF = 'ptrack.urls'
+ROOT_URLCONF = "ptrack.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
     }
 ]
 
-ALLOWED_HOSTS = ['localhost']
-
+ALLOWED_HOSTS = ["localhost"]
 
 # PTRACK SETTINGS
 
-PTRACK_SECRET = 'testptracksecret'
-PTRACK_APP_URL = 'localhost'
+PTRACK_SECRET = "testptracksecret"  # nosec B105
+PTRACK_APP_URL = "localhost"
 USE_AES = True
--- test_ptrack/test_views.py
+++ test_ptrack/test_views.py
@@ -1,3 +1,4 @@
+"""Ptrack test views."""
 import imghdr
 import re
 from tempfile import NamedTemporaryFile
@@ -10,48 +11,37 @@ from django.urls import reverse
 import ptrack
 from ptrack.views import TRANSPARENT_1_PIXEL_GIF
 
-
 # setup
 _test_data_params = [
+    {"args": ["testarg"], "kwargs": {"testkey1": "testarg1", "testkey2": "testarg2"}},
     {
-        'args': ['testarg'],
-        'kwargs': {'testkey1': 'testarg1', 'testkey2': 'testarg2'}
-    },
-    {
-        'args': [
-            'thisisalongtestarg1',
-            'thisisalongtestarg2',
-            'thisisalongtestarg3',
-            'thisisalongtestarg4',
+        "args": [
+            "thisisalongtestarg1",
+            "thisisalongtestarg2",
+            "thisisalongtestarg3",
+            "thisisalongtestarg4",
         ],
-        'kwargs': {
-            'thisisalongtestkey1': 'thisisalongtestkwargvalue1',
-            'thisisalongtestkey2': 'thisisalongtestkwargvalue2',
-        }
+        "kwargs": {
+            "thisisalongtestkey1": "thisisalongtestkwargvalue1",
+            "thisisalongtestkey2": "thisisalongtestkwargvalue2",
+        },
     },
-    {
-        'args': ['[][][][]]', '$%^&%%^$'],
-        'kwargs': {'one': u'[]][]]', 'two': u'[][][]]'}
-    },
-    {
-        'args': ['testarg1', 'testarg2', 'testarg3'],
-        'kwargs': {}
-    },
-    {
-        'args': [],
-        'kwargs': {'testkey1': 'testarg1', 'testkey2': 'testarg2'}
-    },
-
+    {"args": ["[][][][]]", "$%^&%%^$"], "kwargs": {"one": "[]][]]", "two": "[][][]]"}},
+    {"args": ["testarg1", "testarg2", "testarg3"], "kwargs": {}},
+    {"args": [], "kwargs": {"testkey1": "testarg1", "testkey2": "testarg2"}},
 ]
-_test_encrypted_str = 'Ylk67uIkGhy5_ugiHhAgu0_oG72_S-lSGGfHeZOUjJBJZwzNOYruvZqhnQAnGv93td1YvI5K_W-telcEya7vSEosk66TyW00i5lM2_iWAr995vLjyxaL9Z5qBsG3BRec11mS652MhV2x1whSC35VpP63J-WjEP1ejl8AW68cuduH8HdfQSC6draXF7BWuiha706NYnXtESJDACsBJaUJ2aip7qu9JIYsrnKTMUUD7zTI-tqH0lXnxgJEBH2pAz9BPRi5GrjCj4k4xWGUJ6dXvcCQb8RYvH-LThGBjKDwSIkEIZcbNL5tisQWdRpU5xCu9Ig='  # noqa: E501
+_test_encrypted_str = "Ylk67uIkGhy5_ugiHhAgu0_oG72_S-lSGGfHeZOUjJBJZwzNOYruvZqhnQAnGv93td1YvI5K_W-telcEya7vSEosk66TyW00i5lM2_iWAr995vLjyxaL9Z5qBsG3BRec11mS652MhV2x1whSC35VpP63J-WjEP1ejl8AW68cuduH8HdfQSC6draXF7BWuiha706NYnXtESJDACsBJaUJ2aip7qu9JIYsrnKTMUUD7zTI-tqH0lXnxgJEBH2pAz9BPRi5GrjCj4k4xWGUJ6dXvcCQb8RYvH-LThGBjKDwSIkEIZcbNL5tisQWdRpU5xCu9Ig="  # noqa: E501
 template_tag_regex = re.compile(r"src='localhost/(?P<encrypted_data>\S+)/' width=1")
 
 test_record = MagicMock()
-django_engine = engines['django']
+django_engine = engines["django"]
 
 
 class TestTrackingPixel(ptrack.TrackingPixel):
+    """Test Tracking Pixel class."""
+
     def record(self, request, *args, **kwargs):
+        """Record method for test tracking pixel class."""
         test_record(*args, **kwargs)
 
 
@@ -59,59 +49,68 @@ ptrack.tracker.register(TestTrackingPixel)
 
 
 def generate_template_tag_param_str(*args, **kwargs):
-    param_str = u""
+    """Test return query string from supplied args and kwargs."""
+    param_str = ""
     for arg in args:
-        param_str += u"'{}' ".format(arg)
+        param_str += f"'{arg}' "
     for key, value in kwargs.items():
-        param_str += u"{}='{}' ".format(key, value)
+        param_str += f"{key}='{value}' "
     return param_str
 
 
 class PtrackViewsTest(TestCase):
+    """Tests for Ptrack views."""
+
     csrf_checks = False
 
     def test_pixel_image_is_valid(self):
+        """Test if the transparent 1 Pixel GIF file is recognised as a GIF."""
         with NamedTemporaryFile() as fp:
             fp.write(TRANSPARENT_1_PIXEL_GIF)
             fp.flush()
             what = imghdr.what(fp.name)
-        self.assertEqual('gif', what)
+        self.assertEqual("gif", what)
 
     def test_app_is_accessible(self):
-        url = reverse('ptrack', kwargs={'ptrack_encoded_data': _test_encrypted_str})
+        """Test that the URL is accessible and returns a valid response code."""
+        url = reverse("ptrack", kwargs={"ptrack_encoded_data": _test_encrypted_str})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_random_decrypt_fails(self):
-        url = reverse('ptrack', kwargs={'ptrack_encoded_data': 'thisisnotencryptedata'})
+        """
+        Test that random non encrypted data is ignored.
+
+        Note this test will fail due to incorrect length of data.
+        """
+        url = reverse("ptrack", kwargs={"ptrack_encoded_data": "thisisnotencryptedata"})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_registered_tracker_in_trackers(self):
+        """Test that test tracker is registered."""
         matched = False
-        for registered_tracker_name, registered_tracker in ptrack.tracker._registry.items():
+        for _registered_tracker_name, registered_tracker in ptrack.tracker._registry.items():
             if registered_tracker.__class__ == TestTrackingPixel:
                 matched = True
         self.assertEqual(matched, True)
 
     def test_registered_tracker_used(self):
+        """Test that registered trackers are called with valid encrypted data."""
         for test_args in _test_data_params:
-            encrypted_data = ptrack.ptrack_encoder.encrypt(
-                *test_args['args'], **test_args['kwargs']
-            )
-            url = reverse('ptrack', kwargs={'ptrack_encoded_data': encrypted_data})
+            encrypted_data = ptrack.ptrack_encoder.encrypt(*test_args["args"], **test_args["kwargs"])
+            url = reverse("ptrack", kwargs={"ptrack_encoded_data": encrypted_data})
             self.client.get(url)
-            test_record.assert_called_with(*test_args['args'], **test_args['kwargs'])
+            test_record.assert_called_with(*test_args["args"], **test_args["kwargs"])
 
     def test_template_tag(self):
+        """Test that the template tag generates correctly."""
         for test_args in _test_data_params:
-            template_tag_str = generate_template_tag_param_str(
-                *test_args['args'], **test_args['kwargs']
-            )
-            template_str = u"{{% load ptrack %}}{{% ptrack {} %}}".format(template_tag_str)
+            template_tag_str = generate_template_tag_param_str(*test_args["args"], **test_args["kwargs"])
+            template_str = f"{{% load ptrack %}}{{% ptrack {template_tag_str} %}}"
             template_result = django_engine.from_string(template_str).render()
 
             encrypted_str = template_tag_regex.search(template_result).group(1)
             decrypted_data = ptrack.ptrack_encoder.decrypt(encrypted_str)
-            self.assertEqual(decrypted_data[0], test_args['args'])
-            self.assertEqual(decrypted_data[1], test_args['kwargs'])
+            self.assertEqual(decrypted_data[0], test_args["args"])
+            self.assertEqual(decrypted_data[1], test_args["kwargs"])
