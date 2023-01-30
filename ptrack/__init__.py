9c20bff26fa8b705d9385d2a89c0e94019de03ab
campbell <campbell@dxclabs.com>
2023-01-30 07:39:01 +0000

    Remove django 3.2 deprecated default_app_config and use default flag in AppConfig class

--- ptrack/__init__.py
+++ ptrack/__init__.py
@@ -1,5 +1,6 @@
-"""Ptrack Setup"""
+"""Ptrack Setup."""
 import abc
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.module_loading import autodiscover_modules
@@ -7,29 +8,25 @@ from django.utils.module_loading import autodiscover_modules
 from ptrack.encoder import PtrackEncoder
 from ptrack.trackers import tracker
 
-default_app_config = 'ptrack.apps.PtrackConfig'
 ptrack_encoder = PtrackEncoder
 
-if not hasattr(settings, 'PTRACK_SECRET'):
-    raise ImproperlyConfigured('PTRACK_SECRET is not defined')
+if not hasattr(settings, "PTRACK_SECRET"):
+    raise ImproperlyConfigured("PTRACK_SECRET is not defined")
 elif len(settings.PTRACK_SECRET) > 32:
-    raise ImproperlyConfigured('PTRACK_SECRET must be less than 32 bytes')
-
+    raise ImproperlyConfigured("PTRACK_SECRET must be less than 32 bytes")
 
 
 def autodiscover():
-    """
-    Autodiscover pixels.py file in project directory,
-    then register tracker.
-    """
-    autodiscover_modules('pixels', register_to=tracker)
+    """Autodiscover pixels.py file in project directory and register tracker(s)."""
+    autodiscover_modules("pixels", register_to=tracker)
 
 
-class TrackingPixel(object):
+class TrackingPixel:
     """The custom tracking pixel registered by tracker."""
+
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
     def record(self, *args, **kwargs):
-        """Callback executed when the tracking pixel img loads."""
+        """Execute callback when the tracking pixel image loads."""
         raise NotImplementedError("record method has not been implemented")
--- ptrack/apps.py
+++ ptrack/apps.py
@@ -1,19 +1,24 @@
-"""AppConfig Setup"""
+"""AppConfig Setup."""
 from django.apps import AppConfig
 
 
 class SimplePtrackConfig(AppConfig):
     """Simple AppConfig which does not do automatic discovery."""
 
-    name = 'ptrack'
+    name = "ptrack"
+    default = False
 
     def ready(self):
-        super(SimplePtrackConfig, self).ready()
+        """Appconfig no operation on ready."""
+        super().ready()
 
 
 class PtrackConfig(SimplePtrackConfig):
     """The default AppConfig for ptrack which does autodiscovery."""
 
+    default = True
+
     def ready(self):
-        super(PtrackConfig, self).ready()
+        """Appconfig execute autodiscover on ready."""
+        super().ready()
         self.module.autodiscover()
