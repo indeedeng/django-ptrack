1b151e55008844f37503551e83d101cdc8b25b46
campbell <campbell@dxclabs.com>
2023-01-30 07:43:27 +0000

    black reformat urls. Restrict ptrack encoded data to exactly 132 characters.

--- ptrack/urls.py
+++ ptrack/urls.py
@@ -1,7 +1,8 @@
-""" Ptrack Django URL Configuration """
+"""Ptrack Django URL Configuration."""
 from django.urls import re_path
+
 from ptrack.views import TrackingPixel
 
 urlpatterns = [
-    re_path(r'^(?P<ptrack_encoded_data>.*?)/$', TrackingPixel.as_view(), name="ptrack"),
+    re_path(r"^(?P<ptrack_encoded_data>.{132}?)/$", TrackingPixel.as_view(), name="ptrack"),
 ]
